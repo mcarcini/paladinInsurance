e784cae554ffc722134d1b40e23223310e7b19a7
Marco Carcini <marco.carcini@softtek.com>
2020-01-13 17:03:27 -0600

    Initial commit

---
+++ Paladin/Areas/Admin/Controllers/DataExportController.cs
@@ -0,0 +1,62 @@
+﻿using AutoMapper;
+using Paladin.Models;
+using Paladin.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Paladin.Areas.Admin.Controllers
+{
+    public class DataExportController : Controller
+    {
+        private PaladinDbContext _context;
+
+        public DataExportController(PaladinDbContext context)
+        {
+            _context = context;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetQuotesCSV()
+        {
+            var applicants = _context.Applicants.ToList();
+            var mappedApplicants = new List<ApplicantVM>();
+            foreach (var app in applicants)
+            {
+                mappedApplicants.Add(Mapper.Map<ApplicantVM>(app));
+            }
+
+            var stream = new MemoryStream();
+            var streamWriter = new StreamWriter(stream, Encoding.Default);
+
+            foreach (var item in mappedApplicants)
+            {
+                var properties = typeof(ApplicantVM).GetProperties();
+                foreach (var prop in properties)
+                {
+                    streamWriter.Write(GetValue(item, prop.Name));
+                    streamWriter.Write(", ");
+                }
+                streamWriter.WriteLine();
+            }
+
+            streamWriter.Flush();
+            stream.Position = 0;
+
+            return File(stream, "text/csv");
+        }
+
+        public static string GetValue(object item, string propName)
+        {
+            return item.GetType().GetProperty(propName).GetValue(item, null).ToString() ?? "";
+        }
+    }
+}
---
+++ Paladin/Controllers/ServiceController.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using Paladin.Models;
+using Paladin.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml.Serialization;
+
+namespace Paladin.Controllers
+{
+    public class ServiceController : Controller
+    {
+        private PaladinDbContext _context;
+
+        public ServiceController()
+        {
+            _context = new PaladinDbContext();
+        }
+
+        public void GetApplicantsForReminders()
+        {
+            var applicants = _context.Applicants.ToList();
+            var vmApplicants = new List<ApplicantVM>();
+            foreach(var app in applicants)
+            {
+                vmApplicants.Add(Mapper.Map<ApplicantVM>(app));
+            }
+
+            XmlSerializer serializer = new XmlSerializer(vmApplicants.GetType());
+            Response.ContentType = "text/xml";
+            serializer.Serialize(Response.Output, vmApplicants);
+        }
+
+    }
+}
