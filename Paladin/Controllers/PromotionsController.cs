a8dd79f8bad7940cfa8e2973ff86dac5a47a6f66
Marco Carcini <marco.carcini@softtek.com>
2020-03-30 20:30:45 -0600

    added a custom ActionMethodSelectorAttribute

---
+++ Paladin/Controllers/PromotionsController.cs
@@ -0,0 +1,25 @@
+﻿using Paladin.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Paladin.Controllers
+{
+    public class PromotionsController : Controller
+    {
+        //Desktop
+        public ActionResult Promos()
+        {
+            return View();
+        }
+
+        //mobile
+        [IsPaladinMobile]
+        public ActionResult GoPromos()
+        {
+            return View();
+        }
+    }
+}
